84c8a99f28c631ad47ace1a3ab95ace23f799e2d
Shivam Vishwakarma <shivam.vishwakarma@attributics.com>
2024-10-12 15:37:39 +0530

    removed stun server and added twilio turn server

--- src/tinyMEDIA/src/tmedia_session_jsep.js
+++ src/tinyMEDIA/src/tmedia_session_jsep.js
@@ -650,7 +650,7 @@ tmedia_session_jsep01.prototype.__get_lo = function () {
             // o_iceServers = tmedia_session_jsep01.mozThis
             //     ? [{ url: 'stun:23.21.150.121:3478' }, { url: 'stun:216.93.246.18:3478' }, { url: 'stun:66.228.45.110:3478' }, { url: 'stun:173.194.78.127:19302' }]
             //     : [{ url: 'stun:stun.l.google.com:19302' }];
-            o_iceServers = [{ url: 'stun:stun.l.google.com:19302' }];
+            o_iceServers = [{url:"stun:global.stun.twilio.com:3478"}];
         }
         try { tsk_utils_log_info("ICE servers:" + JSON.stringify(o_iceServers)); } catch (e) { }
         this.o_pc = new window.RTCPeerConnection(
